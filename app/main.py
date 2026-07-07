d68f223840cee4cbe6aa6fbee2319985844e442f
rajashekarvs <rajashekarvs555@gmail.com>
2026-07-07 23:41:40 +0530

    Added production url to link frontend

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200", "http://127.0.0.1:4200"],
+    allow_origins=["http://localhost:4200", "http://127.0.0.1:4200", "https://billingsystem-production-33f6.up.railway.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
